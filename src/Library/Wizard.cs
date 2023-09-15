2255e2843eb08f528f22183341569bd15adb0d83
DiazFede <diazfedericoucu@gmail.com>
2023-09-15 16:13:16 -0300

    Wizard

--- src/Library/Wizard.cs
+++ src/Library/Wizard.cs
@@ -7,25 +7,10 @@ namespace RoleplayGame
 {   
     public class Wizard
     {
-<<<<<<< HEAD
         public string Name {get;set;}
         public int Health {get;set;}
         public int Attack=10;
 
-        public Dictionary<>
-        
-        public void ActiveAttack(Characters enemigo)
-        {
-                if enemigo
-
-
-        }
-=======
-        public int Health {get;set;}
-        public int Attack {get;set;}
->>>>>>> 6e62993a66ae754e179f1e29771fdf010f9fc63b
-
-       
         
         
 
