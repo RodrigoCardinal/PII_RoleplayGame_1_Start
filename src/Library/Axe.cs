a0265bd755b6ff28a88ecc572d860fbba6ec2dc7
Joaquin Repetto <joaquin.repetto@correo.ucu.edu.uy>
2023-09-15 16:26:50 -0300

    Se dividen los items por archivos

---
+++ src/Library/Axe.cs
@@ -0,0 +1,8 @@
+namespace RoleplayGame
+{
+    public class Axe
+    {
+        public int attackvalue = 15;
+        public int defensevalue = 0;
+    }
+}
---
+++ src/Library/HeavyArmor.cs
@@ -0,0 +1,8 @@
+namespace RoleplayGame
+{
+    public class HeavyArmor
+    {
+        public int attackvalue = 0;
+        public int defensevalue = 30;
+    }
+}
