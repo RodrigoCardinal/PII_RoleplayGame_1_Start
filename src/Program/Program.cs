d63161a141a736b14c074a2d75af4f621be13f7d
DiazFede <diazfedericoucu@gmail.com>
2023-09-20 10:36:27 -0300

    Programm Ejemplo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,21 +11,23 @@ namespace Program
             Wizard mago = new Wizard ("Mago de prueba");
             Dwarf enano = new Dwarf ("Enano de prueba");
             SpellBook libro = new SpellBook ();
-            
+            Sword Espada = new Sword ();
+            HeavyArmor ArmaduraPesada = new HeavyArmor();
+            LightArmor ArmaduraLigera = new LightArmor();
 
 
-            elfo.AddAttackItems(Sword);
-            elfo.AddDefenseItems (HeavyArmor);
-            elfo.GetHealth(Elf);
-            elfo.GetAttack (Elf);
-            elfo.AttackCharacter(Wizard);
+            elfo.AddAttackItems(Espada);
+            elfo.AddDefensiveItems (ArmaduraPesada);
+            elfo.GetHealth(elfo);
+            elfo.GetAttack (elfo);
+            elfo.AttackCharacter(mago);
 
-            mago.Heal(Wizard);
+            mago.Heal(mago);
             mago.CastSpell ("Fireball" , libro);
-            mago.GetAttack(Wizard);
-            mago.AttackElf (Elf);
-            mago.AddDefenseItems(LightArmor);
-            elfo.GetHealth(Elf);
+            mago.GetAttack(mago);
+            mago.AttackCharacter (elfo);
+            mago.AddDefensiveItems(ArmaduraLigera);
+            elfo.GetHealth(elfo);
 
         }
     }
