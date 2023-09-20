9f53ea9b56a6fd10efa26e433d4eb2b0152957e6
Joaquin Repetto <joaquin.repetto@correo.ucu.edu.uy>
2023-09-20 10:38:45 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,29 +5,29 @@ namespace Program
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string [] args)
         {
             Elf elfo = new Elf ("Elfo de prueba");
             Wizard mago = new Wizard ("Mago de prueba");
             Dwarf enano = new Dwarf ("Enano de prueba");
             SpellBook libro = new SpellBook ();
-            Sword Espada = new Sword ();
-            HeavyArmor ArmaduraPesada = new HeavyArmor();
-            LightArmor ArmaduraLigera = new LightArmor();
+            Sword espada = new Sword ();
+            HeavyArmor armadurapesada = new HeavyArmor ();
+            LightArmor armaduraligera = new LightArmor ();
 
 
-            elfo.AddAttackItems(Espada);
-            elfo.AddDefensiveItems (ArmaduraPesada);
-            elfo.GetHealth(elfo);
+            elfo.AddAttackItems (espada);
+            elfo.AddDefensiveItems (armadurapesada);
+            elfo.GetHealth (elfo);
             elfo.GetAttack (elfo);
-            elfo.AttackCharacter(mago);
+            elfo.AttackCharacter (mago);
 
-            mago.Heal(mago);
+            mago.Heal (mago);
             mago.CastSpell ("Fireball" , libro);
-            mago.GetAttack(mago);
+            mago.GetAttack (mago);
             mago.AttackCharacter (elfo);
-            mago.AddDefensiveItems(ArmaduraLigera);
-            elfo.GetHealth(elfo);
+            mago.AddDefensiveItems (armaduraligera);
+            elfo.GetHealth (elfo);
 
         }
     }
