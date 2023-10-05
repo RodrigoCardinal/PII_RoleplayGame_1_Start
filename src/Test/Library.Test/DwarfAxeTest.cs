2279410b50af5a282cd038436c7ce652fe129dcf
DiazFede <diazfedericoucu@gmail.com>
2023-10-05 18:47:15 -0300

    Tests

--- src/Test/Library.Test/DwarfAxeTest.cs
+++ src/Test/Library.Test/DwarfAxeTest.cs
@@ -3,19 +3,76 @@ using RoleplayGame;
 
 namespace Test.Library
 {
-    public class DwarfAxeTest
+    public class DwarfTest
     {
         [Test]
-        public void InvalidWeaponEquip()
+        public void DwarfAttackCharacter_ReduceCharacterHealth()
         {
+            Dwarf dwarf = new Dwarf("enano");
+            Dwarf targetDwarf = new Dwarf("targetDwarf");
 
-            Dwarf enano = new Dwarf("enano");
-            Axe hacha = new Axe();
+            dwarf.AttackCharacter(targetDwarf);
 
-            bool firstEquipResult = enano.AddAxe(hacha);
-            bool secondEquipResult = enano.AddAxe(hacha);
-            bool expectedSecondEquipResult = false;
-            Assert.AreEqual(expectedSecondEquipResult, secondEquipResult);
+            Assert.AreEqual(150, targetDwarf.Health);
+        }
+
+        [Test]
+        public void DwarfAddAttackItem_IncreaseDwarfAttack()
+        {
+            Dwarf dwarf = new Dwarf("enano");
+            Sword sword = new Sword();
+
+            // Act
+            dwarf.AddAttackItem(sword);
+
+            Assert.AreEqual(35, dwarf.Attack);
+        }
+
+        [Test]
+        public void DwarfRemoveAttackItem_DecreaseDwarfAttack()
+        {
+            Dwarf dwarf = new Dwarf("enano");
+            Sword sword = new Sword();
+            dwarf.AddAttackItem(sword);
+
+            dwarf.RemoveAttackItem(sword);
+
+            Assert.AreEqual(25, dwarf.Attack);
+        }
+
+        [Test]
+        public void DwarfAddDefensiveItem_IncreaseDwarfHealth()
+        {
+            Dwarf dwarf = new Dwarf("enano");
+            LightArmor armor = new LightArmor();
+
+            dwarf.AddDefensiveItem(armor);
+
+            Assert.AreEqual(185, dwarf.Health);
+        }
+
+        [Test]
+        public void DwarfRemoveDefensiveItem_DecreaseDwarfHealth()
+        {
+            Dwarf dwarf = new Dwarf("enano");
+            LightArmor armor = new LightArmor();
+            dwarf.AddDefensiveItem(armor);
+
+            dwarf.RemoveDefensiveItem(armor);
+
+            Assert.AreEqual(175, dwarf.Health);
+        }
+
+        [Test]
+        public void DwarfHeal_IncreaseDwarfHealth()
+        {
+            Dwarf dwarf = new Dwarf("enano");
+            Dwarf injuredDwarf = new Dwarf("injuredDwarf");
+            injuredDwarf.Health = 160;
+
+            dwarf.Heal(injuredDwarf);
+
+            Assert.AreEqual(170, injuredDwarf.Health);
         }
     }
 }
--- src/Test/Library.Test/ElfArmorTest.cs
+++ src/Test/Library.Test/ElfArmorTest.cs
@@ -3,19 +3,76 @@ using RoleplayGame;
 
 namespace Test.Library
 {
-    public class ElfArmorTest
+    public class ElfTest
     {
         [Test]
-        public void InvalidWeaponEquip()
+        public void ElfAttackCharacter_ReduceCharacterHealth()
         {
-
             Elf elfo = new Elf("elfo");
-            LightArmor armaduraliviana = new LightArmor();
+            Elf targetElf = new Elf("targetElf");
 
-            bool firstEquipResult = elfo.AddLightArmor(armaduraliviana);
-            bool secondEquipResult = elfo.AddLightArmor(armaduraliviana);
-            bool expectedSecondEquipResult = false;
-            Assert.AreEqual(expectedSecondEquipResult, secondEquipResult);
+            elfo.AttackCharacter(targetElf);
+
+            Assert.AreEqual(90, targetElf.Health);
+        }
+
+        [Test]
+        public void ElfAddAttackItem_IncreaseElfAttack()
+        {
+            Elf elfo = new Elf("elfo");
+            Sword sword = new Sword();
+
+            // Act
+            elfo.AddAttackItem(sword);
+
+            Assert.AreEqual(20, elfo.Attack);
+        }
+
+        [Test]
+        public void ElfRemoveAttackItem_DecreaseElfAttack()
+        {
+            Elf elfo = new Elf("elfo");
+            Sword sword = new Sword();
+            elfo.AddAttackItem(sword);
+
+            elfo.RemoveAttackItem(sword);
+
+            Assert.AreEqual(10, elfo.Attack);
+        }
+
+        [Test]
+        public void ElfAddDefensiveItem_IncreaseElfHealth()
+        {
+            Elf elfo = new Elf("elfo");
+            LightArmor armor = new LightArmor();
+
+            elfo.AddDefensiveItem(armor);
+
+            Assert.AreEqual(110, elfo.Health);
+        }
+
+        [Test]
+        public void ElfRemoveDefensiveItem_DecreaseElfHealth()
+        {
+            Elf elfo = new Elf("elfo");
+            LightArmor armor = new LightArmor();
+            elfo.AddDefensiveItem(armor);
+
+            elfo.RemoveDefensiveItem(armor);
+
+            Assert.AreEqual(100, elfo.Health);
+        }
+
+        [Test]
+        public void ElfHeal_IncreaseElfHealth()
+        {
+            Elf elfo = new Elf("elfo");
+            Elf injuredElf = new Elf("injuredElf");
+            injuredElf.Health = 90;
+
+            elfo.Heal(injuredElf);
+
+            Assert.AreEqual(100, injuredElf.Health);
         }
     }
 }
